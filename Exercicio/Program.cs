d52fcc1a363801667f5c6db31d6c2536a59bc988
Wendell Rodrigues <robertowendellrodrigues@gmail.com>
2020-11-16 16:39:20 -0300

    Acrescentado messagem de bom dia

--- Exercicio/Program.cs
+++ Exercicio/Program.cs
@@ -4,6 +4,7 @@ namespace Exercicio {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
